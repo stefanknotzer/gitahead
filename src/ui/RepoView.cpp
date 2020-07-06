029d7b0100d137bc16347630d32e421b38ca8e75
Stefan Knotzer <stefan@knotzer.eu>
2020-07-06 18:11:27 +0200

    Build 20200706

--- src/ui/RepoView.cpp
+++ src/ui/RepoView.cpp
@@ -442,8 +442,6 @@ RepoView::RepoView(const git::Repository &repo, MainWindow *parent)
   connect(&mFetchTimer, &QTimer::timeout, [this] {
     fetch(git::Remote(), false, false);
   });
-
-  setLogVisible(true);
 }
 
 RepoView::~RepoView()
